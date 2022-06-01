9089f46ab5461563cc72ada64fa8ac35a743385a
Luisa-Guevara <98427122+Luisa-Guevara@users.noreply.github.com>
2022-05-31 22:34:47 -0500

    Update PROYECTO_FINAL.py

--- PROYECTO_FINAL.py
+++ PROYECTO_FINAL.py
@@ -1,4 +1,6 @@
 import random as r
+import numpy as np
+
 def dado():
     a = r.randrange(1,7)
     return a
@@ -7,84 +9,95 @@ def juego():
     cant_j = int(input('Ingrese el numero de jugadores:\n '))
     apuesta = int(input('Ingrese el valor inicial de la apuesta:\n '))
     
-
-    jugadores = list(range(1,cant_j+1))
+    jugadores = list(np.arange(1,cant_j+1))
     l = {}
     for jugador in jugadores:
         n= 5000-apuesta
         l[jugador] = n 
+        
+    if apuesta < 100:
+        int(input('El valor inicial debe ser mayor a 100, ingrese un nuevo valor inicial de la apuesta:\n '))
     
     pozo= cant_j * apuesta    
-    turno = 1
     
     casino = []
-    
+    turnos = 1
+ 
     while pozo>0:
         lan1 = dado()
         lan2 = dado()
-        
+
         print(l)
         print(' ')
         
-        print('jugador',turno)
+        if turnos>cant_j:
+            turnos = 1
+            print(' ')
+        
+        
+        print('jugador',turnos)
         print('Primer lanzamiento: ',lan1)
         comision = 0.05
- 
         
+
         if lan1 == 1:
             print("Perdiste la apuesta")
             pozo += apuesta
             print ('Pozo:',pozo)
-            l[turno] -= apuesta
-            
+            l[turnos] -= apuesta
+
         if lan1 == 6:
             print("Ganaste la apuesta")
             casino_alerta2 = apuesta*comision
-            l[turno] -= casino_alerta2
-            l[turno] += apuesta
+            l[turnos] -= casino_alerta2
+            l[turnos] += apuesta
             casino.append(casino_alerta2)
             casino = [sum(casino)]
             pozo -= apuesta
             print('Pozo', pozo)
-            
+
         if 1<lan1<6:
             apuesta2 = int(input(f'Ingrese el valor a apostar que no sea mayor a {min(int(pozo),l[jugador])}: '))
             casino_alerta = apuesta2*comision
             print ('Segundo lanzamiento: ',lan2)
+            
             if lan1<lan2:
                 print("Ganaste la apuesta") 
-                l[turno] -= casino_alerta
-                l[turno] += apuesta2
+                l[turnos] -= casino_alerta
+                l[turnos] += apuesta2
                 casino.append(casino_alerta)
                 casino = [sum(casino)]
                 pozo -= apuesta2
-                print('Pozo', pozo)
-            
+                print('Pozo:', pozo)
+
             elif pozo<apuesta:
                 print('Error, la apuesta es mayor que pozo')
                 print('Pozo:',pozo)
-                
+            
             if lan1>=lan2:
                 print("Perdiste la apuesta")
                 pozo = pozo+apuesta2
                 print('Pozo:',pozo)
-                l[turno] -= apuesta2
+                l[turnos] -= apuesta2
                 
-            
-        if turno>=cant_j:
-            turno = 0
-            print(' ')
-            
-        if pozo <= 0 :
+        if l[turnos] <= 0:
+            l.pop(turnos)
             print('Pozo:', int(pozo))
             print('Fin del juego')
-        
-        turno+=1 
-        
-        print ('casino: ',casino)
-        print(' ')
-    
-        if l[jugador] == 0:
-            l.popitem()
+            print('Ganancia de jugadores:',l)
+            break
+
+        if pozo <= 0:
+            print('Pozo:', int(pozo))
+            print('Fin del juego')
+            
+        turnos+=1
+
+            
+        print ('Casino: ',casino)
+        print(' ') 
+
             
 
+
+    return casino
